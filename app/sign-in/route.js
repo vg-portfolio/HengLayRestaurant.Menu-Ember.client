2f45a5b8ad3fcc46fae37d0a4b16d3cdb68c03a0
V4Vince <vince.guoo@gmail.com>
2017-06-29 20:24:21 -0400

    Removes admin-panel from other components

--- app/sign-in/route.js
+++ app/sign-in/route.js
@@ -12,7 +12,7 @@ export default Ember.Route.extend({
   actions: {
     signIn (credentials) {
       return this.get('auth').signIn(credentials)
-      .then(() => this.transitionTo('admin-panel'))
+      .then(() => this.transitionTo('index'))
       .then(() => this.get('flashMessages').success('Thanks for signing in!'))
       .catch(() => {
         this.get('flashMessages')
